c920abe5c535501408a82c398c1cd403287cd8c6
lrovaris <luis.gabrielrovaris@gmail.com>
2023-10-09 19:17:31 -0300

    extra: product simple styling

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,17 +1,38 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Product, Category } from '../shared/app.const';
+import { Product } from '../shared/app.const';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+} from '@ionic/angular';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  providers: [IonCard, IonCardHeader, IonCardTitle, IonCardContent],
   standalone: true,
+  styles: [
+    `
+      .card {
+        border: 1px solid #ddd;
+        padding: 16px;
+        border-radius: 8px;
+        margin: 16px 0;
+      }
+      .card-header {
+        h2,
+        h3 {
+          margin: 0;
+        }
+      }
+    `,
+  ],
 })
 export class ProductComponent implements OnInit {
-
   @Input() product: Product | undefined;
 
   constructor() {}
 
   ngOnInit() {}
-
 }
